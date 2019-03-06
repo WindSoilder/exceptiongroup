678a4606e33cbfc432446886ea6b65123b184e53
WindSoilder <WindSoilder@outlook.com>
2019-03-06 19:26:57 +0800

    change handler script.

--- exceptiongroup/_tests/test_scripts/handler.py
+++ exceptiongroup/_tests/test_scripts/handler.py
@@ -2,25 +2,37 @@
 
 From here we can see the output exception after HandlerChain handled.
 """
-from exceptiongroup import HandlerChain
+from exceptiongroup import HandlerChain, ExceptionGroup
 
 chain = HandlerChain()
 
 
-def raise_error_with_context():
+def raise_error():
+    raise ValueError("Another value error")
+
+
+def raise_another_error():
+    raise RuntimeError("Another runtime error")
+
+
+def raise_group():
     try:
-        raise RuntimeError("This is a runtime error")
-    except Exception:
-        try:
-            raise ValueError("This is a value error")
-        except Exception:
-            raise ZeroDivisionError("Fake zero division error")
+        raise_error()
+    except Exception as e:
+        val_err = e
+
+    try:
+        raise_another_error()
+    except Exception as e:
+        run_err = e
+
+    raise ExceptionGroup("group", [val_err, run_err], [str(val_err), str(run_err)])
 
 
 with chain:
 
-    @chain.handle(ZeroDivisionError)
+    @chain.handle(ValueError)
     def handler(exc):
-        raise
+        raise ZeroDivisionError('zero division error.')
 
-    raise_error_with_context()
+    raise_group()
